64f4ec65494f2f1c82b1d771de609683520908af
Ncesecrede <francescopm95@gmail.com>
2021-06-07 13:46:15 +0100

    Hamburger

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var calculateSum = document.getElementById("calcola");
 var total = document.getElementById("result");
 
+
 calculateSum.addEventListener('click', function(){
     var resultInt = 0 ;
     var checkPrice = document.getElementsByClassName('sum-check');
